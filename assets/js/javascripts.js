3624cd838345a55e12fb894d23ad40a76562ed73
Minhtality <37172196+Minhtality@users.noreply.github.com>
2018-07-09 14:06:58 -0700

    Css styling

--- assets/js/javascripts.js
+++ assets/js/javascripts.js
@@ -1,28 +1,33 @@
- 
-var lis = $("ul li");
-var input  = $("input[type=text]");
-
-
-
-	lis.on("mouseenter",function(){
-		$(this).addClass("bold");
-	});
-
-	lis.on("mouseleave",function(){
-		$(this).removeClass("bold");
-	});
-
-	lis.on("dblclick",function(){
-		$(this).fadeOut(500,function(){
-			$(this).remove();
-		});
-	});
-
-
-input.on("keypress",function(){
-	if(event.which === 13)  {
-		$("ul").append("<li>"+input.val()+"</li>");
-		$(this).val("");
+$("input[type='text']").keypress(function(e) {
+	if (e.which === 13) {
+		var todoText = $(this).val();
+		$(this).val(""); 
+		$("ul").append("<li><span><i class='far fa-trash-alt'></i></span> "+todoText+"</li>");
 	}
-
 });
+
+ $("ul").on("click","li",function(event){
+	$(this).toggleClass('completed');
+
+	event.stopPropagation();
+});
+
+$("ul").on("click","span", function(event) {
+
+	$(this).parent().fadeOut(500,function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+$("ul").on('mouseenter', 'li', function(event) {
+	$(this).addClass('bold')
+	event.stopPropagation();
+	/* Act on the event */
+});
+$("ul").on('mouseleave', 'li', function(event) {
+	$(this).removeClass('bold')
+	event.stopPropagation();
+	/* Act on the event */
+});
+
